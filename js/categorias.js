3b86caa2e1e66c9041dc44abfcd9680e70fb0136
yasmincvlhro <146598330+yasmincvlhro@users.noreply.github.com>
2023-10-01 22:22:46 -0300

    Add files via upload
    Vida marinha funcionando

---
+++ js/categorias.js
@@ -0,0 +1,9 @@
+document.querySelector('.marinha__button').addEventListener('click', () =>{
+  window.open('marinha.html');
+})
+
+
+document.querySelector('.plantas__button').addEventListener('click', () =>{
+  //window.open('game.html');
+  console.log("TESTE");
+})
